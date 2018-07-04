cbac6bc4b240705e8036f6b88a94100a8dd03d63
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2018-07-04 15:54:55 +0200

    Video PopUp fullscreen.

--- src/components/popup/popup.vue
+++ src/components/popup/popup.vue
@@ -29,8 +29,8 @@ export default {
 <style scoped lang="scss">
 .popup {
     background-color: rgba($color-black, 0.75);
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     position: fixed;
     top: 0;
     left: 0;
